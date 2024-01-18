5227065a921f84b50596ff030c1de615e918619d
Rinku <rkd302012@gmail.com>
2024-01-18 14:43:55 +0530

    again....

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import jsCookie from 'js-cookie';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'js-cookie': jsCookie,
-    },
-  },
 });
-
